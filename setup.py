d841d7b9bef78ffaf1d9eada7808100cd77d4ce7
jokenwang <602525419@qq.com>
2018-01-06 21:27:21 +0800

    添加打包配置

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- encoding=utf-8 -*-
+
+# description:
+# author:jack
+# create_time: 2018/1/6
+
+"""
+    desc:pass
+"""
+
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='Bot',
+    version='0.2.1',
+    author='jack',
+    author_email='xxxxxxx',
+    description='Third party DuerOS Bot Python SDK',
+    keywords='DuerOS Bot',
+    url='https://github.com/jokenwang/bot-sdk-python',
+    packages=find_packages(),
+    install_requires=[
+        'pycrypto>=2.6.1'
+    ]
+)
