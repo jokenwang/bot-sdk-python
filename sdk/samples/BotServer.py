4dc280dc65651e989f27eaa086663a9847069ec4
jokenwang <602525419@qq.com>
2018-01-12 22:46:23 +0800

    添加新的调试代码

--- sdk/samples/BotServer.py
+++ sdk/samples/BotServer.py
@@ -13,22 +13,38 @@ import json
 from sdk.samples.Bot import Bot
 
 def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
     try:
         request_body_size = int(environ.get('CONTENT_LENGTH', 0))
     except(ValueError):
         request_body_size = 0
 
-    request_body = environ['wsgi.input'].read(request_body_size)
+    request_body = environ['wsgi.input'].read(request_body_size).decode('utf-8')
     print(type(request_body))
+    if not request_body:
+        return ['未获取到请求数据']
+
     bot = Bot(request_body)
+    #添加错误回调方法
+    bot.setCallBack(callback)
+
     #验证签名enableVerifyRequestSign  disableVerifyRequestSign 关闭验证签名
-    bot.initCertificate(environ).enableVerifyRequestSign()
+    # bot.initCertificate(environ).enableVerifyRequestSign()
+    # bot.initCertificate(environ).disableVerifyRequestSign()
+
     body_str = bot.run()
 
     body = body_str.encode('utf-8')
+
+    response_headers = [('Content-Type', 'application/json'),
+                        ('Content-Length', str(len(body)))]
+    status = '200 OK'
+
+    start_response(status, response_headers)
+
     return [body]
 
+def callback(data):
+    print(data)
 def askContol():
         response = '''
         {
