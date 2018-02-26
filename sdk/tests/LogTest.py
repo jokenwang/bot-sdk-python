f78141bf5fe8bbd9e58df7d9731726ad1fa0e70c
wangninghb@gmail.com <ning.wang-2@jia007.com>
2018-02-26 22:56:54 +0800

    添加日志输出工具类

---
+++ sdk/tests/LogTest.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- encoding=utf-8 -*-
+
+# description:
+# author:jack
+# create_time: 2018/2/26
+
+"""
+    desc:pass
+"""
+
+
+class LogTest:
+    pass
+
+
+if __name__ == '__main__':
+    pass
