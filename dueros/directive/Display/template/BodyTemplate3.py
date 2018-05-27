a52c2412c71ce5f15491117a59d3923ed0e58970
jokenwang <wangninghb@gmail.com>
2018-05-27 21:29:48 +0800

    添加 支持屏幕展示模板

---
+++ dueros/directive/Display/template/BodyTemplate3.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- encoding=utf-8 -*-
+
+# description:
+# author:jack
+# create_time: 2018/5/26
+
+"""
+    desc:pass
+"""
+
+from dueros.directive.Display.template.TextImageTemplate import TextImageTemplate
+
+
+class BodyTemplate3(TextImageTemplate):
+
+    def __init__(self):
+        super(BodyTemplate3, self).__init__('BodyTemplate3')
+
+
+if __name__ == '__main__':
+    pass
