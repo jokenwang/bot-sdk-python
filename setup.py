02152a1b71720f3c748816bac4777056e3fb6afe
jokenwang <wangninghb@gmail.com>
2018-07-30 13:35:58 +0800

    fix

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import find_packages
 
 setup(
     name='dueros-bot-python2',
-    version='1.2.5',
+    version='1.2.6',
     author='jack',
     author_email='mupdf@sina.com',
     description='Third party DuerOS Bot Python SDK ',
