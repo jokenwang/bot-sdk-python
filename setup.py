fd13c066e0da3035b12b8649660de91221572c64
jokenwang <wangninghb@gmail.com>
2018-11-10 11:58:07 +0800

    完善README

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import find_packages
 
 setup(
     name='dueros-bot-master-alpha',
-    version='1.1.1',
+    version='2.1.2',
     author='jack',
     author_email='mupdf@sina.com',
     description='Third party DuerOS Bot Python SDK',
