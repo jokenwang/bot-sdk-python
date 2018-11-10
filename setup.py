807c0d162a89a1ee27422ee2e90d3a13a5c657b1
jokenwang <wangninghb@gmail.com>
2018-11-10 10:16:15 +0800

    修复权限bug

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import find_packages
 
 setup(
     name='dueros-bot',
-    version='2.1.0',
+    version='2.1.1',
     author='jack',
     author_email='mupdf@sina.com',
     description='Third party DuerOS Bot Python SDK',
